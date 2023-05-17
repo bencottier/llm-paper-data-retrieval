1f08353cb4df7250b904d9f620ad6432da5cbb8e
Ben Cottier <ben.cottier@invenialabs.co.uk>
2023-05-17 17:45:21 +0100

    Update save_paper_txts.py

--- save_paper_txts.py
+++ save_paper_txts.py
@@ -6,15 +6,18 @@ from pdfminer.high_level import extract_text
 os.makedirs('input_data', exist_ok=True)
 
 # Download dataset from the Parameters, Compute and Data Trends in ML sheet
-df = pd.read_csv('https://docs.google.com/spreadsheets/d/1AAIebjNsnJj_uKALHbXNfn3_YsT6sHXtCU0q7OIPuc4/export?format=csv#gid=0')
+df = pd.read_csv('https://docs.google.com/spreadsheets/d/1beiholXkLpiYoeyDL64bOHLI-mXZ-_8ZnY1Ed6Sfjh0/export?format=csv#gid=0')
 
-year_start = 2017
+reversed_df = df.iloc[::-1]
+df = pd.concat([reversed_df[:10], reversed_df[40:50], reversed_df[80:90]])
 
-# Recode columns
-df['Publication date'] = pd.to_datetime(df['Publication date'], errors='coerce', dayfirst=True)
+# year_start = 2017
 
-# Filter for papers of only the last 5 years
-df = df[df['Publication date'] > f'{year_start}-01-01']
+# # Recode columns
+# df['Publication date'] = pd.to_datetime(df['Publication date'], errors='coerce', dayfirst=True)
+
+# # Filter for papers of only the last 5 years
+# df = df[df['Publication date'] > f'{year_start}-01-01']
 
 # Keep only bibliographical data
 df = df.filter(['Author(s)', 'Publication date', 'Reference', 'Link'])
@@ -25,7 +28,10 @@ df = df[df['Link'].str.contains('(arxiv|.pdf$)', regex=True)]
 keys = ['Number of hardware units', 'Hardware model', 'Training FLOP/s']
 
 # Enable for test running with the first ten papers
-df = df[:10]
+# df = df[:10]
+
+print(df['Reference'].values)
+
 # Or a specific paper
 # idx = 3
 # df = df[idx:idx+1]
@@ -36,6 +42,12 @@ for i, row in df.iterrows():
     # replace "abs" with "pdf" in arxiv url links
     url = url.replace('abs', 'pdf')
     print(f"Looking into \"{row['Reference']}\"")
+
+    paper_title = row['Reference'].replace(' ', '_').replace(':', '').replace('"', '').lower()
+    filename = 'input_data/' + paper_title + '.txt'
+    if os.path.exists(filename):
+        print(f"File {filename} already exists, skipping")
+        continue
 
     try:
         response = requests.get(url)
@@ -50,6 +62,5 @@ for i, row in df.iterrows():
 
     text = extract_text('download.pdf')
 
-    paper_title = row['Reference'].replace(' ', '_').replace(':', '').replace('"', '').lower()
-    with open('input_data/' + paper_title + '.txt', 'w') as f:
+    with open(filename, 'w') as f:
         f.write(text)
